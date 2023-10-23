009cc85239de3fc5b68b724c0c5251e471bc2a1c
NgocIsHere <111295106+NgocIsHere@users.noreply.github.com>
2023-10-23 23:26:27 +0700

    Update FAT32BootSector.py

--- FAT32BootSector.py
+++ FAT32BootSector.py
@@ -1,47 +1,83 @@
 import os
-import sys
 
-boot_sector_size = 512
+BYTE_P_SECTOR = 512
+mft_cluster = None
+    
+def read_vbr(drive_letter):
+    try:
+        drive_path = fr'\\.\{drive_letter}:'
+
+        with open(drive_path, 'rb') as f:
+            # Read the VBR data
+            boot_sector_data = f.read(512)
+            
+            return boot_sector_data
+
+    except FileNotFoundError:
+        print(f"Error: Drive identifier {drive_letter} not found.")
+    except PermissionError:
+        print("Error: Permission denied. Run the script with appropriate privileges.")
 
 
-#Read
-try:
-    with open("\\.D:",'rb') as f:
-        boot_sector_data = f.read(boot_sector_size)
-except FileNotFoundError:
-    print(f"Error: Drive identifier not found.")
-except PermissionError:
-    print("Error: Permission denied. Run the script with appropriate privileges.")
+######################################## MAIN  
+drive_letter = "D"
 
-# Init
-jmp = boot_sector_data[0:2]
-OEM_ID = boot_sector_data[3:10].decode() 
-bytesPerSector = int.from_bytes(boot_sector_data[11:12])
-sectorsPerCluster = int(boot_sector_data[13])
-reservedSectors = boot_sector_data[14:15]
-fatCopiesNum = boot_sector_data[16]
-# 17:20: Not used
-MediaDescriptor = boot_sector_data[21]
-# 22:23: Not used
-sectorPerTrack = int.from_bytes(boot_sector_data[24:25],"little")
-heads = int.from_bytes(boot_sector_data[26:27],"little")
-sectorsOnDisk =  int.from_bytes(boot_sector_data[28:31],"little")
-sectorsInPar = int.from_bytes(boot_sector_data[32:35],"little")
-sectorsPerFat = int.from_bytes(boot_sector_data[36:39],"little")
-fatHandlingFlags = boot_sector_data[40:41]
-#driveVersion = boot_sector_data[42:43].decode()
-clusterNumForStartRootTable = int.from_bytes(boot_sector_data[44:47],"little")
-sectorsNumFromSPartition1= boot_sector_data[48:49] # for the File System Information Sector
-sectorsNumFromSPartition2 = boot_sector_data[50:51] #for the Backup Boot Sector
-reversed = boot_sector_data[52:63]
-logicalDriveNumber =boot_sector_data[64]
-currentHead = boot_sector_data[65]
-signature = boot_sector_data[66]
-id = boot_sector_data[67:70]
-volumeLabel = boot_sector_data[71:81]
-systemID = boot_sector_data[82:89]
+# READ DISK
+boot_sector_data = read_vbr(drive_letter)
 
-#Print:
-print(f"OEM Name: {OEM_ID}")
-print(f"bytesPerSector: {bytesPerSector}")
-print(f"sectorsPerCluster: {sectorsPerCluster}")
+# INIT
+jmp = boot_sector_data[0:3]
+OEM_ID = boot_sector_data[3:11].decode("utf-8") 
+bytesPerSector = int.from_bytes(boot_sector_data[11:13],byteorder='little')
+sectorsPerCluster = int.from_bytes(boot_sector_data[13:14],'little')
+reservedSectors = int.from_bytes(boot_sector_data[14:16],'little')
+fatsNum = int.from_bytes(boot_sector_data[16:17],'little')
+# 17:21: Not used
+mediaDescriptor = boot_sector_data[21:22].hex()
+# 22:24: Not used
+sectorPerTrack = int.from_bytes(boot_sector_data[24:26],"little")
+headsNum = int.from_bytes(boot_sector_data[26:28],"little")
+hiddenSectors =  int.from_bytes(boot_sector_data[28:32],"little")
+totalSectors = int.from_bytes(boot_sector_data[32:36],"little")
+sectorsPerFat = int.from_bytes(boot_sector_data[36:40],"little")
+fatHandlingFlags = int.from_bytes(boot_sector_data[40:41],"little")
+driveVersion = int.from_bytes(boot_sector_data[42:44],"little")
+clusterNumForStartRootTable = int.from_bytes(boot_sector_data[44:48],"little")
+systemInformation = int.from_bytes(boot_sector_data[48:50],"little") # for the File System Information Sector
+backUpBootSector =int.from_bytes(boot_sector_data[50:52],"little") #for the Backup Boot Sector
+reversed =int.from_bytes(boot_sector_data[52:63],"little")
+physicalDrive = int.from_bytes(boot_sector_data[64:65],"little")
+reverved =int.from_bytes(boot_sector_data[65:66],"little")
+signature =int.from_bytes(boot_sector_data[66:67],"little")
+id = boot_sector_data[67:71].hex()
+volumeLabel = boot_sector_data[71:82].decode("utf-8")
+systemID = boot_sector_data[82:90].decode("utf-8")
+
+# PRINT 
+print("\t ================ Information ==================")
+print("JMP Instruction:", ' '.join(['{:02X}'.format(byte) for byte in jmp]))
+print("OEM Name:", OEM_ID)
+print("\n\t\tBIOS Parameter Block (BPB) Details:\n")
+print("Bytes Per Sector: ", bytesPerSector)
+print("Sectors Per Cluster: ", sectorsPerCluster)
+print("Reserved sector: ", reservedSectors)
+print("Number of FATs: ", fatsNum)
+print("Media Descriptor: ", mediaDescriptor)
+print("Sectors per track: ", sectorPerTrack)
+print("Number of heads: ", headsNum)
+print("Hidden sectors: ", hiddenSectors)
+print("Total sector: ", totalSectors)
+print("Sectors per FAT: ", sectorsPerFat)
+print("Extended flags: ",fatHandlingFlags)
+print("Version: ", driveVersion)
+print("Root cluster: ", clusterNumForStartRootTable)
+print("System Information: ", systemInformation)
+print("Backup Boot Sector: ", backUpBootSector)
+
+print("\n\t\tExtended BIOS Parameter Block Details:\n")
+print("Physical dirve: ", physicalDrive)
+print("Reverved: ",reversed)
+print("Extended signature: ", signature)
+print("Serial number: ", id)
+print("Volume label: ", volumeLabel)
+print("File system: ",systemID)
