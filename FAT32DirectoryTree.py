16a65ec28be4f0ad5479bac12ac6b32f88992417
NgocIsHere <111295106+NgocIsHere@users.noreply.github.com>
2023-10-28 14:27:02 +0700

    attributes

--- FAT32DirectoryTree.py
+++ FAT32DirectoryTree.py
@@ -1,4 +1,20 @@
 import os
+import datetime
+import struct
+
+
+def byteToBits(value, start_index, bit_cnt):
+    return (value & (2 ** start_index - 1)) >> (start_index - bit_cnt)
+
+def getDateTimeFromDosTime(dos_date, dos_time, dos_tenth_of_second):
+    created_year = byteToBits(dos_date, 16, 7) + 1980
+    created_month = byteToBits(dos_date, 9, 4)
+    created_day = byteToBits(dos_date, 5, 5)
+    created_hour = byteToBits(dos_time, 16, 5)
+    created_minute = byteToBits(dos_time, 11, 6)
+    created_second = int(byteToBits(dos_time, 5, 5) * 2 + dos_tenth_of_second / 100)
+    creatied_milisecond = (dos_tenth_of_second % 100) * 10000
+    return datetime.datetime(created_year, created_month, created_day, created_hour, created_minute, created_second, creatied_milisecond)
 
 bytes_per_sector = 0
 sectors_per_cluster = 0
@@ -24,31 +40,57 @@ def read_vbr(drive_path):
         except PermissionError:
             print("Error: Permission denied. Run the script with appropriate privileges.")
 
-def read_file(file_path):
+def print_infor_dir(dir_entry):
+        # Name and extension:
+        # Attributes
+        READ_ONLY = dir_entry[11] >> 0 & 1
+        HIDDEN = dir_entry[11] >> 1 & 1
+        SYSTEM = dir_entry[11] >> 2 & 1
+        VOLUME = dir_entry[11] >> 3 & 1
+        DIRECTORY = dir_entry[11] >> 4 & 1
+        ARCHIVE = dir_entry[11] >> 5 & 1
 
-    try:
-        with open(file_path, 'rb') as f:
-            boot_sector_data = f.read(512)
-            return boot_sector_data
+        # TIME
+        ## Created time
+        created_tenth_of_second = int.from_bytes(dir_entry[13:14],'little')
+        created_time = int.from_bytes(dir_entry[14:16], 'little')
+        created_date = int.from_bytes(dir_entry[16:18], byteorder='little')
+        if created_date !=0 and created_time!= 0 and created_tenth_of_second != 0:
+            created_time = getDateTimeFromDosTime(created_date, created_time, created_tenth_of_second)
 
-    except FileNotFoundError:
-        print(f"Error: Drive identifier {file_path} not found.")
-    except PermissionError:
-        print("Error: Permission denied. Run the script with appropriate privileges.")
+        ## Access time
+        lastAccess = int.from_bytes(dir_entry[18:22], 'little')
+        if lastAccess != 0:
+            lastAccess = getDateTimeFromDosTime(lastAccess, 0, 0)
 
+        ## Modified time
+        modifiedTime = int.from_bytes(dir_entry[22:24], 'little')
+        modifiedDate = int.from_bytes(dir_entry[24:26], 'little')
+        if modifiedDate!=0 and modifiedTime!=0:
+            modifiedDate = getDateTimeFromDosTime(modifiedDate, modifiedTime, 0)
+        # Print the attributes
+        print("*************************************************")
+        print("Attributes:\t")
+        print(f"\tREAD_ONLY: {READ_ONLY}")
+        print(f"\tHIDDEN: {HIDDEN}")
+        print(f"\tSYSTEM: {SYSTEM}")
+        print(f"\tVOLUME: {VOLUME}")
+        print(f"\tDIRECTORY: {DIRECTORY}")
+        print(f"\tARCHIVE: {ARCHIVE}")
+        print(f"Created Time: {created_time}")
+        print(f"Last Access: {lastAccess}")
+        print(f"Modified Date: {modifiedDate}")
 
-def build_folder_tree(root_cluster, drive_path):
-
+def build_folder_tree(root_cluster, drive_path, flag):
     global bytes_per_sector
     global sectors_per_cluster
     global reserved_sectors
     global num_fats
     global sectors_per_fat
     global bytes_per_dir_entry
-
+    global detail 
     root_dir_offset = (reserved_sectors + num_fats * sectors_per_fat + (root_cluster - 2) * sectors_per_cluster) * bytes_per_sector
     folder_dict = {}
-
     def read_sub_entry(entry):
         name = ""
         for i in range(1, 31):
@@ -59,7 +101,15 @@ def build_folder_tree(root_cluster, drive_path):
                 else:
                     name += chr(byte)
         return name
+    
+    
 
+    if flag == 1:
+        with open(drive_path, 'rb') as f1:
+            f1.seek(root_dir_offset)
+            sector1 = f1.read(32)
+            print(sector1)
+            print_infor_dir(sector1)
     with open(drive_path, 'rb') as f:
         f.seek(root_dir_offset)
         main_entry_name = ""
@@ -92,7 +142,7 @@ def build_folder_tree(root_cluster, drive_path):
                             folder_dict[main_entry_name] = int.from_bytes(dir_entry[26:27])
                             main_entry_name = ""
                         else:
-                            folder_name = bytes(dir_entry[:8]).decode(errors='ignore').strip()
+                            folder_name = bytes(dir_entry[:11]).decode(errors='ignore').strip()
                             folder_dict[folder_name] = int.from_bytes(dir_entry[26:27])
 
                     elif dir_entry[11] == 0x20:  # File entry
@@ -105,23 +155,44 @@ def build_folder_tree(root_cluster, drive_path):
                             if file_name != '':  # Skip empty file entries
                                 file_name_with_extension = f"{file_name}.{extension}"
                                 folder_dict[file_name_with_extension] = int.from_bytes(dir_entry[26:27])
+                                if flag == 2:
+                                    print_infor_dir(dir_entry)
+                                    print("********************* DATA *********************")
+                                    numbytes = int.from_bytes(dir_entry[20:22])
+                                    root_dir_offset = (reserved_sectors + num_fats * sectors_per_fat + (root_cluster - 2) * sectors_per_cluster) * bytes_per_sector
+                                    print(root_dir_offset)
+                                    print(numbytes)
+                                else:
+                                    continue
 
                     elif dir_entry[11] == 0x16:  # System File
                         if len(main_entry_name) > 0:
                             main_entry_name = ""
                         else:
                             file_name = bytes(dir_entry[:11]).decode(errors='ignore').strip()
+               
 
+                
 def loop(name, root_cluster, drive_path):
     new_prompt = f"{name}:> "
     set_command_prompt(new_prompt)
-    folder_dict = build_folder_tree(root_cluster, drive_path)
-
+    folder_dict = build_folder_tree(root_cluster, drive_path, 0)
     while True:
         command = input(new_prompt)
+        for key in folder_dict.keys():
+            print(key)
+        print()
         if command.lower() == "exit":
             break
-
+        if command.lower() == "info":
+            print(f"Bytes per sector: {bytes_per_sector}")
+            print(f"Sectors per cluster: {sectors_per_cluster}")
+            print(f"Reserved: {reserved_sectors}")
+            print(f"Numbers of FATs: {num_fats}")
+            print(f"Sectors per FAT: {sectors_per_fat}")
+            print(f"Root cluster: {root_cluster}")
+            folder_dict = build_folder_tree(root_cluster, drive_path, 1)
+        
         if command.lower() == "ls":
             for key in folder_dict.keys():
                 print(key)
@@ -130,6 +201,7 @@ def loop(name, root_cluster, drive_path):
         if command in folder_dict:
             print()
             loop(command, folder_dict[command], drive_path)
+
 
 
 def start_program():
@@ -143,7 +215,7 @@ def start_program():
     global num_fats
     global sectors_per_fat
     global bytes_per_dir_entry
-
+    
     bytes_per_sector = int.from_bytes(sector[11:13], 'little')
     sectors_per_cluster = int.from_bytes(sector[13:14], "little")
     reserved_sectors = int.from_bytes(sector[14:16], "little")
@@ -153,19 +225,20 @@ def start_program():
 
     new_prompt = f"{name}:> "
     set_command_prompt(new_prompt)
-
-    folder_dict = build_folder_tree(root_cluster, drive_path)
-
+    checkFile = 0
+    folder_dict = build_folder_tree(root_cluster, drive_path, 0)
+    
     while True:
         command = input(new_prompt)
-        if command.lower() == "Info":
+        if command.lower() == "info":
             print(f"Bytes per sector: {bytes_per_sector}")
             print(f"Sectors per cluster: {sectors_per_cluster}")
             print(f"Reserved: {reserved_sectors}")
             print(f"Numbers of FATs: {num_fats}")
             print(f"Sectors per FAT: {sectors_per_fat}")
             print(f"Root cluster: {root_cluster}")
-
+            build_folder_tree(root_cluster, drive_path, 1)
+            
         if command.lower() == "exit":
             break
 
@@ -176,6 +249,11 @@ def start_program():
 
         if command in folder_dict:
             print()
-            loop(command, folder_dict[command], drive_path)
+            if command.find("TXT") >= 0:
+                build_folder_tree(root_cluster, drive_path, 2)
+            else:
+                loop(command, folder_dict[command], drive_path)
+
+        
 
 start_program()
